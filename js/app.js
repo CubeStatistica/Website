c84e84f8934dafa087d2943bb81824a7df5768f0
Suneeta Devi <64021031+SuneetaDevi@users.noreply.github.com>
2022-06-23 10:19:12 +0500

    testimonial/CS-132

--- js/app.js
+++ js/app.js
@@ -19,7 +19,19 @@ $(document).ready(function () {
                 nav: true,
             }
         }
-    })
+    });
+       // Back to top button
+       $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    // $('.back-to-top').click(function () {
+    //     $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+    //     return false;
+    // });
 
     $('#projects-slider').owlCarousel({
         loop: true,
