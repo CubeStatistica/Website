6760160ade9f6c3dff0ac26e4ba8a257460cf9a1
Suneeta Devi <64021031+SuneetaDevi@users.noreply.github.com>
2022-08-11 17:58:26 +0500

    Updated Course banner

--- assets/js/main.js
+++ assets/js/main.js
@@ -345,3 +345,75 @@ $('#projects-slider').owlCarousel({
   }
 })
 });
+
+function initHeroSlider()
+	{
+		if($('.hero_slider').length)
+		{
+			var owl = $('.hero_slider');
+
+			owl.owlCarousel(
+			{
+				items:1,
+				loop:true,
+				smartSpeed:800,
+				autoplay:true,
+				nav:false,
+				dots:false
+			});
+
+			// add animate.css class(es) to the elements to be animated
+			function setAnimation ( _elem, _InOut )
+			{
+				// Store all animationend event name in a string.
+				// cf animate.css documentation
+				var animationEndEvent = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+
+				_elem.each ( function ()
+				{
+					var $elem = $(this);
+					var $animationType = 'animated ' + $elem.data( 'animation-' + _InOut );
+
+					$elem.addClass($animationType).one(animationEndEvent, function ()
+					{
+						$elem.removeClass($animationType); // remove animate.css Class at the end of the animations
+					});
+				});
+			}
+
+			// Fired before current slide change
+			owl.on('change.owl.carousel', function(event)
+			{
+				var $currentItem = $('.owl-item', owl).eq(event.item.index);
+				var $elemsToanim = $currentItem.find("[data-animation-out]");
+				setAnimation ($elemsToanim, 'out');
+			});
+
+			// Fired after current slide has been changed
+			owl.on('changed.owl.carousel', function(event)
+			{
+				var $currentItem = $('.owl-item', owl).eq(event.item.index);
+				var $elemsToanim = $currentItem.find("[data-animation-in]");
+				setAnimation ($elemsToanim, 'in');
+			})
+
+			// Handle Custom Navigation
+			if($('.hero_slider_left').length)
+			{
+				var owlPrev = $('.hero_slider_left');
+				owlPrev.on('click', function()
+				{
+					owl.trigger('prev.owl.carousel');
+				});
+			}
+
+			if($('.hero_slider_right').length)
+			{
+				var owlNext = $('.hero_slider_right');
+				owlNext.on('click', function()
+				{
+					owl.trigger('next.owl.carousel');
+				});
+			}
+		}	
+	}
