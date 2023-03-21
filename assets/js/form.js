bd25342fcea69cd617fa61ca504cb0552a829344
Suneeta Devi <suneetadevi779@gmail.com>
2023-03-20 23:10:33 -0700

    Added title and company in email.

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,21 +1,25 @@
-function sendMail(){
+function sendMail() {
     var params = {
-        name : document.getElementById("name").value,
-        message : document.getElementById("message").value
+        title: document.getElementById("title").value,
+        name: document.getElementById("name").value,
+        company: document.getElementById("company").value,
+        message: document.getElementById("message").value
     };
-    
-const serviceID = "service_36dx4hr";
-const templateID = "template_wdbg4jl";
 
-emailjs.send(serviceID, templateID, params)
-.then(
-    res =>{
-        document.getElementById("name").value = "";
-        document.getElementById("message").value = "";
+    const serviceID = "service_36dx4hr";
+    const templateID = "template_wdbg4jl";
 
-        console.log(res);
-        alert("Your Message Sent Successfully");
-    }
-)
-.catch((err) =>console.log(err));
+    emailjs.send(serviceID, templateID, params)
+        .then(
+            res => {
+                document.getElementById("title").value = "";
+                document.getElementById("name").value = "";
+                document.getElementById("message").value = "";
+                document.getElementById("company").value = "";
+
+                console.log(res);
+                alert("Your Message Sent Successfully");
+            }
+        )
+        .catch((err) => console.log(err));
 }
