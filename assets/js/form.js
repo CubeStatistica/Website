6edf19ac28322ab2e40751b8080de92c1451243b
Suneeta Devi <suneetadevi779@gmail.com>
2023-03-20 23:31:01 -0700

    Added validations on contact form.

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,3 +1,8 @@
+var name = document.getElementById("name").value;
+var title = document.getElementById("title").value;
+var company = document.getElementById("company").value;
+var message = document.getElementById("message").value;
+
 function sendMail() {
     var params = {
         title: document.getElementById("title").value,
@@ -12,13 +17,19 @@ function sendMail() {
     emailjs.send(serviceID, templateID, params)
         .then(
             res => {
-                document.getElementById("title").value = "";
-                document.getElementById("name").value = "";
-                document.getElementById("message").value = "";
-                document.getElementById("company").value = "";
+                if (name != "" && message != "" && company != "" && message != "") {
+                    document.getElementById("title").value = "";
+                    document.getElementById("name").value = "";
+                    document.getElementById("message").value = "";
+                    document.getElementById("company").value = "";
+                    console.log(res);
+                    document.getElementById("sent-message").innerHTML = "Your Message Sent Successfully";
+                    alert("Your Message Sent Successfully");
+                }
+                else {
+                    document.getElementById("error-message").innerHTML = "Above fields are required";
+                }
 
-                console.log(res);
-                alert("Your Message Sent Successfully");
             }
         )
         .catch((err) => console.log(err));
