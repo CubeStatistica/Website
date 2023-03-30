c1f4d70b70aa2eb81d87062a377c5f85a4436cb4
Suneeta Devi <suneetadevi779@gmail.com>
2023-03-30 09:13:47 -0700

    Added email in form.js.

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,28 +1,34 @@
 function sendMail() {
-    var name = document.getElementById("name").value;
-    var title = document.getElementById("title").value;
+    var firstname = document.getElementById("firstname").value;
+    var lastname = document.getElementById("lastname").value;
+    var email = document.getElementById("email").value;
+    var position = document.getElementById("position").value;
     var company = document.getElementById("company").value;
     var message = document.getElementById("message").value;
 
     // alert("name = " + name);
-    if (name == "" && company == "" && message == "") {
+    if (firstname == "" && lastname == "" && email == "" && position == "" && company == "" && message == "") {
         document.getElementById("error-message").innerHTML = "Above fields are required";
         // alert("Fields are required");
     }
-    else if (name != "" && company != "" && message != "") {
+    else if (firstname != "" && lastname != "" && email != "" && position != "" && company != "" && message != "") {
         var params = {
-            title: document.getElementById("title").value,
-            name: document.getElementById("name").value,
+            firstname: document.getElementById("firstname").value,
+            lastname: document.getElementById("lastname").value,
+            email: document.getElementById("email").value,
+            position: document.getElementById("position").value,
             company: document.getElementById("company").value,
             message: document.getElementById("message").value
         };
-        const serviceID = "service_0lebw28";
-        const templateID = "template_x2g7dmv";
+        const serviceID = "service_36dx4hr";
+        const templateID = "template_wdbg4jl";
         emailjs.send(serviceID, templateID, params)
             .then(
                 res => {
-                    document.getElementById("title").value = "";
-                    document.getElementById("name").value = "";
+                    document.getElementById("firstname").value = "";
+                    document.getElementById("lastname").value = "";
+                    document.getElementById("email").value = "";
+                    document.getElementById("position").value = "";
                     document.getElementById("message").value = "";
                     document.getElementById("company").value = "";
                     console.log(res);
